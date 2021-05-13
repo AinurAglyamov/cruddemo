01d8615d859b71dac529e063803e02c7bcfe1e58
Aynur Aglyamov <Aynur.Aglyamov@x5.ru>
2021-05-13 23:26:42 +0500

    Applying Spring Data Rest

--- src/main/java/ru/aynur/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/ru/aynur/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package ru.aynur.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.aynur.springboot.cruddemo.entity.Employee;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
